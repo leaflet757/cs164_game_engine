b5f899450ef52ab0141c241ea88d67d8ebc90b34
Leif Myer <chatsboy@aol.com>
2014-04-20 01:56:46 -0700

    test again

--- Minigolf Game Engine/Minigolf Game Engine/Main.cpp
+++ Minigolf Game Engine/Minigolf Game Engine/Main.cpp
@@ -1,310 +1,147 @@
-// opengltest.cpp : Defines the entry point for the console application.
-//
+/*
+* OpenGLSamples (openglsamples.sf.net) Examples
+* VC++ users should create a Win32 Console project and link
+* the program with glut32.lib, glu32.lib, opengl32.lib
+*
+* GLUT can be downloaded from http://www.xmission.com/~nate/glut.html
+* OpenGL is by default installed on your system.
+* For an installation of glut on windows for MS Visual Studio 2010 see: http://nafsadh.wordpress.com/2010/08/20/glut-in-ms-visual-studio-2010-msvs10/
+*
+*/
 
-#include<Windows.h>
-#include<gl/GL.h>
-#include<gl/GLU.h>
-#include<gl/glut.h>
-#include<math.h>
+#include <stdio.h>
+#include <windows.h>	   // Standard header for MS Windows applications
+#include <GL/gl.h>		   // Open Graphics Library (OpenGL) header
+#include <GL/glut.h>	   // The GL Utility Toolkit (GLUT) Header
 
-void  display_obj(void);
-void  draw_string(float, float, float, char *);
-void  handle_menu(int);
-void  handle_motion(int, int);
-void  handle_mouse(int, int, int, int);
-
-typedef enum {
-	TRANSLATE,
-	ROTATE_X,
-	ROTATE_Y,
-	ROTATE_Z
-} mode;
-
-int    btn[3] = { 0 };		// Current button state
-mode   cur_mode = TRANSLATE;		// Current mouse mode
-float  translate[3] = { 0 };		// Current translation values
-float  rotate[3] = { 0 };		// Current rotation values
-int    mouse_x, mouse_y;		// Current mouse position
+#define KEY_ESCAPE 27
 
 
-void init(void)
+
+typedef struct {
+	int width;
+	int height;
+	char* title;
+
+	float field_of_view_angle;
+	float z_near;
+	float z_far;
+} glutWindow;
+
+glutWindow win;
+
+float g_rotation = 0;
+float g_rotation_speed = 0.2f;
+
+void display()
 {
-	glClearColor(0.0, 0.0, 0.0, 0.0);
-	glShadeModel(GL_FLAT);
-}
-
-
-void checkerboard()
-{
-	// define all vertices   X     Y     Z
-	GLfloat v0[3], v1[3], v2[3], v3[3], delta;
-	int color = 0;
-
-	delta = 0.5f;
-
-	// define the two colors
-	GLfloat color1[3] = { 0.9f, 0.9f, 0.9f };
-	GLfloat color2[3] = { 0.05f, 0.05f, 0.05f };
-
-	v0[1] = v1[1] = v2[1] = v3[1] = 0.0f;
-
-	glBegin(GL_QUADS);
-
-	for (int x = -5; x <= 5; x++)
-	{
-		for (int z = -5; z <= 5; z++)
-		{
-			glColor3fv((color++) % 2 ? color1 : color2);
-
-			v0[0] = 0.0f + delta*z;
-			v0[2] = 0.0f + delta*x;
-			v0[1] = -2.0f;
-
-			v1[0] = v0[0] + delta;
-			v1[2] = v0[2];
-			v1[1] = -2.0f;
-
-			v2[0] = v0[0] + delta;
-			v2[2] = v0[2] + delta;
-			v2[1] = -2.0f;
-
-			v3[0] = v0[0];
-			v3[2] = v0[2] + delta;
-			v3[1] = -2.0f;
-
-			glVertex3fv(v0);
-			glVertex3fv(v1);
-			glVertex3fv(v2);
-			glVertex3fv(v3);
-		}
-	}
-	glEnd();
-}
-
-void display_obj()
-{
-	float	 c[4][3] = {		// Axis colours
-		{ 1, 0, 0 },
-		{ 0, 1, 0 },
-		{ 0, 0, 1 },
-		{ 1, 1, 1 }
-	};
-	int	 i;				// Loop counters
-	char  *txt[3] = {			// Axis labels
-		"+X", "+Y", "+Z"
-	};
-
-	float theta;
-
-	float  v[4][3] = {		// Vertex positions
-		{ 1, 0, 0 },
-		{ 0, 1, 0 },
-		{ 0, 0, 1 },
-		{ 0, 0, 0 }
-	};
-
-
-	glMatrixMode(GL_MODELVIEW);		// Setup model transformations
+	// Clear Screen and Depth Buffer
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glLoadIdentity();
+
+	// Define a viewing transformation
+	gluLookAt(4, 2, 0, 0, 0, 0, 0, 1, 0);
+
+
+	// Push and pop the current matrix stack. 
+	// This causes that translations and rotations on this matrix wont influence others.
 
 	glPushMatrix();
-	glTranslatef(translate[0], translate[1], translate[2]);
-	glRotatef(rotate[0], 1, 0, 0);
-	glRotatef(rotate[1], 0, 1, 0);
-	glRotatef(rotate[2], 0, 0, 1);
+	glColor3f(1, 0, 0);
+	glTranslatef(0, 0, 0);
+	glRotatef(g_rotation, 0, 1, 0);
+	glRotatef(90, 0, 1, 0);
 
-	glColor3f(1, 1, 0);
-	//glutWireCube( 2.0 );
-	glutWireDodecahedron();
-
-	for (i = 0; i < 3; i++) {		// Draw axis at center of cube
-		glBegin(GL_LINES);
-		glColor3fv(c[i]);
-		glVertex3fv(v[i]);
-		glColor3fv(c[3]);
-		glVertex3fv(v[3]);
-		glEnd();
-	}
-
-	glColor3f(1, 1, 1);
-
-	draw_string(v[0][0], v[0][1], v[0][2], txt[0]);
-	draw_string(v[1][0], v[1][1], v[1][2], txt[1]);
-	draw_string(v[2][0], v[2][1], v[2][2], txt[2]);
-
+	// Draw the teapot
+	glutSolidTeapot(1);
 	glPopMatrix();
 
-	checkerboard();
 
-	glFlush();				// Flush OpenGL queue
-
-	glutSwapBuffers();			// Display back buffer
-
+	g_rotation += g_rotation_speed;
+	glutSwapBuffers();
 }
 
 
-void reshape(int w, int h)
+void initialize()
 {
-	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+	// select projection matrix
 	glMatrixMode(GL_PROJECTION);
+
+	// set the viewport
+	glViewport(0, 0, win.width, win.height);
+
+	// set matrix mode
+	glMatrixMode(GL_PROJECTION);
+
+	// reset projection matrix
 	glLoadIdentity();
-	glFrustum(-1.0, 1.0, -1.0, 1.0, 1.5, 20.0);
+	GLfloat aspect = (GLfloat)win.width / win.height;
+
+	// set up a perspective projection matrix
+	gluPerspective(win.field_of_view_angle, aspect, win.z_near, win.z_far);
+
+	// specify which matrix is the current matrix
 	glMatrixMode(GL_MODELVIEW);
+	glShadeModel(GL_SMOOTH);
+
+	// specify the clear value for the depth buffer
+	glClearDepth(1.0f);
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+
+	// specify implementation-specific hints
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+
+	GLfloat amb_light[] = { 0.1, 0.1, 0.1, 1.0 };
+	GLfloat diffuse[] = { 0.6, 0.6, 0.6, 1 };
+	GLfloat specular[] = { 0.7, 0.7, 0.3, 1 };
+	glLightModelfv(GL_LIGHT_MODEL_AMBIENT, amb_light);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuse);
+	glLightfv(GL_LIGHT0, GL_SPECULAR, specular);
+	glEnable(GL_LIGHT0);
+	glEnable(GL_COLOR_MATERIAL);
+	glShadeModel(GL_SMOOTH);
+	glLightModeli(GL_LIGHT_MODEL_TWO_SIDE, GL_FALSE);
+	glDepthFunc(GL_LEQUAL);
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	glClearColor(0.0, 0.0, 0.0, 1.0);
 }
 
-void keyboard(unsigned char key, int x, int y)
+
+void keyboard(unsigned char key, int mousePositionX, int mousePositionY)
 {
 	switch (key)
 	{
-	case 27:
+	case KEY_ESCAPE:
 		exit(0);
+		break;
+
+	default:
 		break;
 	}
 }
 
-void draw_string(float x, float y, float z, char *txt)
-
-//  This routine draws the text string at the given (x,y,z) position
-//
-//  x,y,z:  Raster position for text
-//  txt:    String to draw
+int main(int argc, char **argv)
 {
-	glRasterPos3f(x, y, z);
-	while (*txt != '\0') {
-		glutBitmapCharacter(GLUT_BITMAP_9_BY_15, *txt);
-		txt++;
-	}
-}					// End routine draw_string
+	// set window values
+	win.width = 640;
+	win.height = 480;
+	win.title = "OpenGL/GLUT Example. Visit http://openglsamples.sf.net ";
+	win.field_of_view_angle = 45;
+	win.z_near = 1.0f;
+	win.z_far = 500.0f;
 
-
-void handle_menu(int ID)
-
-//  This routine handles popup menu selections
-//
-//  ID:  Menu entry ID
-{
-	switch (ID) {
-	case 0:				// XY translation
-		cur_mode = TRANSLATE;
-		break;
-	case 1:				// X rotation
-		cur_mode = ROTATE_X;
-		break;
-	case 2:				// Y rotation
-		cur_mode = ROTATE_Y;
-		break;
-	case 3:				// Z rotation
-		cur_mode = ROTATE_Z;
-		break;
-	case 4:				// Quit
-		exit(0);
-	}
-}					// End routine handle_menu
-
-
-void handle_motion(int x, int y)
-
-//  This routine acts as a callback for GLUT mouse motion events
-//
-//  x, y:  Cursor position
-{
-	float	 x_ratchet;			// X ratchet value
-	float	 y_ratchet;			// Y ratchet value
-
-
-	if (!btn[0]) {			// Left button not depressed?
-		return;
-	}
-
-	x_ratchet = glutGet(GLUT_WINDOW_WIDTH) / 10.0;
-	y_ratchet = glutGet(GLUT_WINDOW_HEIGHT) / 10.0;
-
-	//  Windows XP has y = 0 at top, GL has y = 0 at bottom, so reverse y
-
-	y = glutGet(GLUT_WINDOW_HEIGHT) - y;
-
-	switch (cur_mode) {
-	case TRANSLATE:			// XY translation
-		translate[0] += (float)(x - mouse_x) / x_ratchet;
-		translate[1] += (float)(y - mouse_y) / y_ratchet;
-		break;
-	case ROTATE_X:			// X rotation
-		x_ratchet /= 10.0;
-		rotate[0] += (float)(x - mouse_x) / x_ratchet;
-		break;
-	case ROTATE_Y:			// Y rotation
-		x_ratchet /= 10.0;
-		rotate[1] += (float)(x - mouse_x) / x_ratchet;
-		break;
-	case ROTATE_Z:			// Z rotation
-		x_ratchet /= 10.0;
-		rotate[2] += (float)(x - mouse_x) / x_ratchet;
-		break;
-	}
-
-	mouse_x = x;				// Update cursor position
-	mouse_y = y;
-
-	glutPostRedisplay();
-}					// End routine handle_motion
-
-
-void handle_mouse(int b, int s, int x, int y)
-
-//  This routine acts as a callback for GLUT mouse events
-//
-//  b:     Mouse button (left, middle, or right)
-//  s:     State (button down or button up)
-//  x, y:  Cursor position
-{
-	if (s == GLUT_DOWN) {		// Store button state if mouse down
-		btn[b] = 1;
-	}
-	else {
-		btn[b] = 0;
-	}
-
-	mouse_x = x;
-	mouse_y = glutGet(GLUT_WINDOW_HEIGHT) - y;
-}					// End routine handle_mouse
-
-
-
-int _tmain(int argc, char* argv[])
-{
-	glutInit(&argc, argv);
-
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-	glutCreateWindow("GLUT Demo");
-
-	glutDisplayFunc(display_obj);	// Setup GLUT callbacks
-	glutMotionFunc(handle_motion);
-	glutMouseFunc(handle_mouse);
-
-	glutCreateMenu(handle_menu);	// Setup GLUT popup menu
-	glutAddMenuEntry("Translate", 0);
-	glutAddMenuEntry("Rotate X", 1);
-	glutAddMenuEntry("Rotate Y", 2);
-	glutAddMenuEntry("Rotate Z", 3);
-	glutAddMenuEntry("Quit", 4);
-	glutAttachMenu(GLUT_RIGHT_BUTTON);
-
-	glMatrixMode(GL_PROJECTION);	// Setup perspective projection
-	glLoadIdentity();
-	gluPerspective(70, 1, 1, 40);
-
-	glMatrixMode(GL_MODELVIEW);		// Setup model transformations
-	glLoadIdentity();
-	gluLookAt(0, 0, 5, 0, 0, -1, 0, 1, 0);
-
-	glShadeModel(GL_SMOOTH);
-
-	glClearDepth(1.0);			// Setup background colour
-	glClearColor(0, 0, 0, 0);
-	glEnable(GL_DEPTH_TEST);
-
-	glutMainLoop();			// Enter GLUT main loop
-
-
+	// initialize and run program
+	glutInit(&argc, argv);                                      // GLUT initialization
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);  // Display Mode
+	glutInitWindowSize(win.width, win.height);				// set window size
+	glutCreateWindow(win.title);					// create Window
+	glutDisplayFunc(display);						// register Display Function
+	glutIdleFunc(display);						// register Idle Function
+	glutKeyboardFunc(keyboard);						// register Keyboard Handler
+	initialize();
+	glutMainLoop();							// run GLUT mainloop
 	return 0;
 }
